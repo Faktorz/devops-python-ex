1ab05c9ed87daec4c086560c01439ea37379ff9d
John McCawley <john.mccawley@thedigitalgarage.io>
2018-04-08 13:29:47 -0500

    add prometheus client

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from middleware import setup_metrics
+import prometheus_client
+
+CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 
 application = Flask(__name__)
-
 setup_metrics(application)
 
 @application.route("/")
@@ -21,6 +23,10 @@ def test1():
 @application.errorhandler(500)
 def handle_500(error):
     return str(error), 500
+
+@app.route('/metrics')
+def metrics():
+    return Response(prometheus_client.generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     application.run()
