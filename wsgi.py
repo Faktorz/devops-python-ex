e3321dd6b4bdc638d35c5cbe3271b25e6b85d367
John McCawley <john.mccawley@thedigitalgarage.io>
2018-04-08 17:12:43 -0500

    test route

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,9 @@ application = Flask(__name__)
 def hello():
     return "Hello New World!"
 
+@application.route("/test")
+def hello():
+    return "This is a test!"
+
 if __name__ == "__main__":
     application.run()
