7772ccf9946b4935e2c19160f9ea20e57a8d98a5
John McCawley <john.mccawley@thedigitalgarage.io>
2018-04-08 12:31:42 -0500

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from middleware import setup_metrics
+
 application = Flask(__name__)
+
+setup_metrics(application)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@application.route('/test/')
+def test():
+    return 'rest'
+
+@application.route('/test1/')
+def test1():
+    1/0
+    return 'rest'
+
+@application.errorhandler(500)
+def handle_500(error):
+    return str(error), 500
 
 if __name__ == "__main__":
     application.run()
