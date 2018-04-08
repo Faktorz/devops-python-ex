cfd5a41039071b0dc949fca8e4f3a5450165e7dc
John McCawley <john.mccawley@thedigitalgarage.io>
2018-04-08 15:15:57 -0500

    revert

--- wsgi.py
+++ wsgi.py
@@ -1,27 +1,9 @@
-import os
-from flask import Flask, Response
-from prometheus_client import multiprocess
-from prometheus_client import generate_latest, CollectorRegistry, CONTENT_TYPE_LATEST, Gauge, Counter
-
+from flask import Flask
 application = Flask(__name__)
 
-IN_PROGRESS = Gauge("inprogress_requests", "Example gauge", multiprocess_mode='livesum')
-NUM_REQUESTS = Counter("num_requests", "Example counter")
-
-
 @application.route("/")
-@IN_PROGRESS.track_inprogress()
 def hello():
-    NUM_REQUESTS.inc()
-    return "Hello World from {}!".format(os.getpid())
-
-
-@application.route("/metrics")
-def metrics():
-    registry = CollectorRegistry()
-    multiprocess.MultiProcessCollector(registry)
-    data = generate_latest(registry)
-    return Response(data, mimetype=CONTENT_TYPE_LATEST)
+    return "Hello New World!"
 
 if __name__ == "__main__":
     application.run()
